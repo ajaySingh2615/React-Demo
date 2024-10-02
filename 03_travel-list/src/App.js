a3565c411a4c1d23802d9caed6b7009a8515e6c5
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-02 23:19:57 +0530

    calculating statistics as drived state

--- 03_travel-list/src/App.js
+++ 03_travel-list/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -112,11 +112,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your paking list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentPacked = Math.round((numPacked / numItems) * 100) || 0;
+
   return (
     <footer className="stats">
       <em>
-        💼 You have X items on your list, and you already packed X (X%) items{" "}
+        {percentPacked === 100
+          ? "You got everything! Ready to go ✈️"
+          : `💼 You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentPacked}%) items`}
       </em>
     </footer>
   );
