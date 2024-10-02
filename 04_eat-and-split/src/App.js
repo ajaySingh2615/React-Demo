021d78f24c42e1ded5a4286994c1e185f5d57bf2
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-03 01:48:09 +0530

    displaying the new friend form

--- 04_eat-and-split/src/App.js
+++ 04_eat-and-split/src/App.js
@@ -1,15 +1,24 @@
+import React from "react";
 import Button from "./components/Button";
 import FormAddFriend from "./components/FormAddFriend";
 import FormSplitBill from "./components/FormSplitBill";
 import FriendList from "./components/FriendList";
 
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = React.useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendList />
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
 
       <FormSplitBill />
