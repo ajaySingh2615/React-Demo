26c568c26a42492baf58f725338817613089764c
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-04 23:39:27 +0530

    Cleaning up the titles

--- 05_usepopcorn/src/components/MovieDetail.js
+++ 05_usepopcorn/src/components/MovieDetail.js
@@ -62,6 +62,10 @@ function MovieDetail({ selectedId, onCloseMovie, onAddWatched, watched }) {
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = "usePopcorn";
+      };
     },
     [title]
   );
