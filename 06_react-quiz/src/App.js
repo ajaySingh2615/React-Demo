cb8753e66ad2fe51c4ce4a5b5acd74ed67549322
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-09 01:00:42 +0530

    Add Question component and implement quiz start functionality in StartScreen component

--- 06_react-quiz/src/App.js
+++ 06_react-quiz/src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useReducer } from "react";
 import Loader from "./components/Loader";
 import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
 
 const initialState = {
   questions: [],
@@ -18,6 +19,8 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Action Unknown Error");
   }
@@ -42,7 +45,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
