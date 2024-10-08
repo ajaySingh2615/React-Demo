d2c430865382410a45446d35a6fd3a42271f9492
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-09 00:39:33 +0530

    Add json-server for API simulation and implement basic app structure with useReducer
    - Added `json-server` as a dependency and configured a script to run it for serving questions data.
    - Implemented `useReducer` in `App.js` to manage application state and handle data fetching.
    - Created `Header` and `Main` components for basic app layout.
    - Added `Main.js` component to render children within a main section.
    - Updated `App.js` to fetch questions from the local server and display initial UI structure.

--- 06_react-quiz/src/App.js
+++ 06_react-quiz/src/App.js
@@ -1,9 +1,43 @@
-import DateCounter from "./components/DateCounter";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action Unknown Error");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   );
 }
