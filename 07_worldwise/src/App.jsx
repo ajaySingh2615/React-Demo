13163b777fe82a57d4a6b7066867ab363c2cb0b2
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-10 01:08:46 +0530

    Implementing the city list

--- 07_worldwise/src/App.jsx
+++ 07_worldwise/src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CityList from "./components/City/CityList.jsx";
 import AppLayout from "./pages/AppLayout/AppLayout.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import Login from "./pages/Login/Login.jsx";
@@ -6,7 +8,28 @@ import PageNotFound from "./pages/PageNotFound/PageNotFound.jsx";
 import Pricing from "./pages/Pricing/Pricing.jsx";
 import Product from "./pages/Product/Product.jsx";
 
+const BASE_URL = "http://localhost:8080";
+
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -15,8 +38,14 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities </p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
---
+++ 07_worldwise/src/components/City/CityList.jsx
@@ -0,0 +1,23 @@
+import Message from "../Message/Message";
+import Spinner from "../Spinner/Spinner";
+import CityItem from "./CityItem";
+import styles from "./CityList.module.css";
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CityList;
