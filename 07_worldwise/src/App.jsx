833e5ef9eed66cd23e2e820d2126ad64066b3088
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-09 22:34:53 +0530

    using css modules and building the pages and components

--- 07_worldwise/src/App.jsx
+++ 07_worldwise/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage.jsx";
-import PageNotFound from "./pages/PageNotFound.jsx";
-import Pricing from "./pages/Pricing.jsx";
-import Product from "./pages/Product.jsx";
+import AppLayout from "./pages/AppLayout/AppLayout.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import Login from "./pages/Login/Login.jsx";
+import PageNotFound from "./pages/PageNotFound/PageNotFound.jsx";
+import Pricing from "./pages/Pricing/Pricing.jsx";
+import Product from "./pages/Product/Product.jsx";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
